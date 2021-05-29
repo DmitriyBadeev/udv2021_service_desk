b5e00ea60d324d4b06ca0a2c1898f3ca7d65c5b1
Dmitriy2Belikov <d.belik@bk.ru>
2021-05-29 06:07:56 +0500

    Added background license checking

--- src/ServiceDesk.Api/DependencyInstaller.cs
+++ src/ServiceDesk.Api/DependencyInstaller.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Hangfire;
+using Hangfire.Common;
+using Hangfire.SqlServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using ServiceDesk.Api.BackgroundTasks.ErrorFilter;
+using ServiceDesk.Api.Services.BackgroundTasks;
 using ServiceDesk.Api.Services.FilesManager;
 using ServiceDesk.Core.Entities.PersonalAreaSystem;
 using ServiceDesk.Infrastructure;
@@ -31,8 +36,26 @@ namespace ServiceDesk.Api
                 .UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
                 .LogTo(Console.WriteLine, LogLevel.Information));
 
+            var options = new SqlServerStorageOptions
+            {
+                SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                QueuePollInterval = TimeSpan.Zero,
+                CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                UseRecommendedIsolationLevel = true,
+                DisableGlobalLocks = true
+            };
+
+            services.AddHangfire(x => x
+                .UseSqlServerStorage(configuration.GetConnectionString("HangfireConnection"), options)
+                .UseSerializerSettings(new JsonSerializerSettings
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                    PreserveReferencesHandling = PreserveReferencesHandling.Objects
+                }));
+
             services.AddTransient<IEnumHelper, EnumHelper>();
             services.AddTransient<IFilesManager, FilesManager>();
+            services.AddTransient<ILicenseCheckerService, LicenseCheckerService>();
 
             services.AddErrorFilter<GraphQlErrorFilter>();
 
@@ -59,6 +82,19 @@ namespace ServiceDesk.Api
                     .Where(type => type.Name.EndsWith("Handler")))
                 .AsImplementedInterfaces()
                 .WithTransientLifetime());
+
+            RegisterBackgroundTasks(services);
+        }
+
+        private void RegisterBackgroundTasks(IServiceCollection services)
+        {
+            JobStorage.Current = new SqlServerStorage(configuration.GetConnectionString("HangfireConnection"));
+
+            var licenseCheckerService = services
+                .BuildServiceProvider()
+                .GetRequiredService<ILicenseCheckerService>();
+
+            RecurringJob.AddOrUpdate(() => licenseCheckerService.LicenseCheck(), Cron.Daily(22));
         }
     }
 }
